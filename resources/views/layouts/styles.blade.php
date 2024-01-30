9088b94a237f7fc643d01f47d943b2cdf74cd87e
Firdaus Ishaddin <firdausishaddin@gmail.com>
2024-01-31 03:30:33 +0800

    update

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -9,25 +9,7 @@
 <link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
 <link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
 <!--end::Global Stylesheets Bundle-->
-<link href="assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet" type="text/css"/>
-<link href="//cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap.css rel="stylesheet" type="text/>css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap.min.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap4.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap4.min.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap5.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap5.min.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.bulma.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.bulma.min.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.foundation.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.foundation.min.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.jqueryui.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.jqueryui.min.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.semanticui.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/buttons.semanticui.min.css rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/common.scss rel="stylesheet" type="text/css" />
-<link href="//cdn.datatables.net/buttons/2.4.2/css/mixins.scss rel="stylesheet" type="text/css" />
+<link href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
+<link href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
 
 @stack('styles')

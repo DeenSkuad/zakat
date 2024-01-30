d2045680e4f8f3eefcbaa58141f7a4262226a6a3
Firdaus Ishaddin <firdausishaddin@gmail.com>
2024-01-30 13:27:37 +0800

    fix seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -17,43 +17,43 @@ class UsersSeeder extends Seeder
     {
         $data = [
             [
-                'ic_no' => '00000',
-                'name' => 'Admin',
+                'ic_no' => '11111',
+                'name' => 'Ahmad Admin',
                 'email' => 'admin@test.com',
                 'email_verified_at' => Carbon::now(),
                 'password' => bcrypt('asdasd')
             ],
             [
-                'ic_no' => '11111',
-                'name' => 'Zakat HoD',
+                'ic_no' => '22222',
+                'name' => 'Ahmad Zakat HoD',
                 'email' => 'zakathod@test.com',
                 'email_verified_at' => Carbon::now(),
                 'password' => bcrypt('asdasd')
             ],
             [
-                'ic_no' => '22222',
-                'name' => 'Amil',
+                'ic_no' => '33333',
+                'name' => 'Ahmad Amil',
                 'email' => 'amil@test.com',
                 'email_verified_at' => Carbon::now(),
                 'password' => bcrypt('asdasd')
             ],
             [
-                'ic_no' => '33333',
-                'name' => 'Pembantu Amil',
+                'ic_no' => '44444',
+                'name' => 'Ahmad Pembantu Amil',
                 'email' => 'pembantuamil@test.com',
                 'email_verified_at' => Carbon::now(),
                 'password' => bcrypt('asdasd')
             ],
             [
-                'ic_no' => '44444',
-                'name' => 'Asnaf',
+                'ic_no' => '55555',
+                'name' => 'Ahmad Asnaf',
                 'email' => 'asnaf@test.com',
                 'email_verified_at' => Carbon::now(),
                 'password' => bcrypt('asdasd')
             ],
             [
-                'ic_no' => '55555',
-                'name' => 'Pembayar Zakat',
+                'ic_no' => '66666',
+                'name' => 'Ahmad Pembayar Zakat',
                 'email' => 'pembayarzakat@test.com',
                 'email_verified_at' => Carbon::now(),
                 'password' => bcrypt('asdasd')
